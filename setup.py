f7c4ff87ded81079a271c923bc2da8b311c91382
Didion, John (NIH/NHGRI) [F] <john.didion@nih.gov>
2015-07-24 14:54:43 -0400

    update dev status

--- setup.py
+++ setup.py
@@ -27,10 +27,10 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
 
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: GPL',
+        'License :: OSI Approved :: GPLv3',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
